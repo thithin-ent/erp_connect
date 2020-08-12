20f696b298cad1b654daa02eb5b001d93cb92293
thithin-ent <65296325+thithin-ent@users.noreply.github.com>
2020-08-12 15:29:43 +0900

    Update erp_control_sub.py

--- erp42_control/src/erp_control_sub.py
+++ erp42_control/src/erp_control_sub.py
@@ -26,13 +26,12 @@ class ERP42_Serial():
 		self.alive = 0
 		
 		rospy.init_node('erp42_sub', anonymous=True)
-		rospy.Subscriber('/twist_cmd', TwistStamped, self.speedCallback)
+		rospy.Subscriber('/erp42/speed', Int16, self.speedCallback)
 		rospy.Subscriber('/erp42/brake', UInt8, self.brakeCallback)
 		rospy.Subscriber('/erp42/gear', UInt8, self.gearCallback)
 		rospy.Subscriber('/erp42/auto', UInt8, self.autoCallback)
 		rospy.Subscriber('/erp42/e_stop', UInt8, self.e_stopCallback)
-		rospy.Subscriber('/twist_cmd', TwistStamped, self.ctrl_Callback)
-		rospy.Subscriber('/detection/image_detector/objects', DetectedObjectArray, self.label_read)
+		rospy.Subscriber('/erp42/steer', Int16, self.ctrl_Callback)
 
 		#pub = rospy.Publisher('/test', UInt8 , queue_size = 10)
 	def sub_to_serial(self):
@@ -45,7 +44,7 @@ class ERP42_Serial():
 		rospy.sleep(0.1)
 
 	def speedCallback(self, msg):
-		self.speed = msg.twist.linear.x
+		self.speed = msg.data
 
 	def brakeCallback(self, msg):
 		self.brake = msg.data
@@ -60,23 +59,14 @@ class ERP42_Serial():
 		self.e_stop = msg.data
 
 	def ctrl_Callback(self, msg):
-		self.steering = -msg.twist.angular.z
-
-	def label_read(self,msg):
-		la = np.array(msg.objects)
-		print(la[0].label)
+		self.steering = msg.data
 
 	def create_erp42_cmd_packet(self):
-		steer = self.steering*180/3.141592
-		if steer > 28 :
-			steer = 28
-		elif steer < -28 :
-			steer = -28
 		fmt = '>BBBBBBHhBBBB'
 		self.packet_bytes_cmd = struct.pack(fmt, 0x53, 0x54, 0x58
-			,self.auto, self.e_stop, self.gear, int(self.speed*10), int(steer*71), self.brake, self.alive
+			,self.auto, self.e_stop, self.gear, int(self.speed*10), int(self.steer*71), self.brake, self.alive
 			,0xD, 0xA)
-		self.aux_data = [self.auto, self.e_stop, self.gear, self.speed, steer, self.brake, self.alive]
+		self.aux_data = [self.auto, self.e_stop, self.gear, self.speed, self.steer, self.brake, self.alive]
 		#print(self.aux_data)
 		self.alive += 1
 		if self.alive == 256:
