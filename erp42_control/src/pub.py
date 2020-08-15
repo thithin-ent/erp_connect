ebdc5192a5f8374c79986a274e4844f6b0d7db1a
thithin-ent <65296325+thithin-ent@users.noreply.github.com>
2020-08-15 11:50:55 +0900

    Update pub.py

--- erp42_control/src/pub.py
+++ erp42_control/src/pub.py
@@ -5,14 +5,15 @@ import time
 import struct
 from std_msgs.msg import UInt8, Int16
 from sensor_msgs.msg import Image
-from autoware_msgs.msg import TwistStamped, DetectedObjectArray
+from autoware_msgs.msg import DetectedObjectArray
+from geometry_msgs.msg import TwistStamped
 import sys
 import os
 import rospy
 import message_filters
 import cv2
 import numpy as np
-from cv-bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge, CvBridgeError
 
 class ERP42_Control():
 	def __init__(self):
@@ -23,6 +24,7 @@ class ERP42_Control():
 		self.la = 'none'
 		self.count = 0
 		self.lineimage_d = 0
+		self.cv_image = None
 		
 		rospy.init_node('erp42_pub', anonymous=True)
 		rospy.Subscriber('/twist_cmd', TwistStamped, self.speedCallback)
@@ -43,34 +45,9 @@ class ERP42_Control():
 		self.steering = -msg.twist.angular.z
 		
 	def image_Callback(self, msg):
-		cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-		mark = np.copy(cv_image)
-		# white detect
-		blue_threshold = 200
-		green_threshold = 200
-		red_threshold = 200
-		bgr_threshold = [blue_threshold, green_threshold, red_threshold]
-		thresholds = (cv_image[:,:,0] < bgr_threshold[0])  | (cv_image[:,:,1] < bgr_threshold[1]) | (cv_image[:,:,2] < bgr_threshold[2])
-		mark[thresholds] = [0,0,0]
+		self.cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-					
-		gray = cv2.cvtColor(mark,cv2.COLOR_BGR2GRAY) 
-		canny = cv2.Canny(gray, 5000, 1500, apertureSize = 5, L2gradient = True)
-		lines = cv2.HoughLinesP(canny, 0.8, np.pi / 180, 90, minLineLength = 10, maxLineGap = 100)
-
-		for i in lines:
-			if abs(i[0][1] - i[0][3]) < 20: #y pixel diff
-				self.lineimage_d = 1
-				cv2.line(cv_image, (i[0][0], i[0][1]), (i[0][2], i[0][3]), (255, 0, 0), 2)
-
-		cv2.imshow("test", mark)
-		cv2.imshow("test2", canny)
-		cv2.imshow("test3", image)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-		
-
-	def self.label_Callback(self,msg):
+	def label_Callback(self,msg):
 		if msg.objects:
 			self.la = msg.objects[0].label
 		else :
@@ -127,33 +104,34 @@ class ERP42_Control():
 		elif self.mode == 4:
 				print('mode 4')
 				#image = cv2.imread("/home/kingofgps/test.jpeg", cv2.IMREAD_ANYCOLOR)
-				#mark = np.copy(image)
-				# white detect
-				#blue_threshold = 200
-				#green_threshold = 200
-				#red_threshold = 200
-				#bgr_threshold = [blue_threshold, green_threshold, red_threshold]
-				#thresholds = (image[:,:,0] < bgr_threshold[0])  | (image[:,:,1] < bgr_threshold[1]) | (image[:,:,2] < bgr_threshold[2])
-				#mark[thresholds] = [0,0,0]
+				if type(self.cv_image) != type(None) :
+					mark = np.copy(self.cv_image)
+					print('msdfsdf')
+					# white detect
+					blue_threshold = 200
+					green_threshold = 200
+					red_threshold = 200
+					bgr_threshold = [blue_threshold, green_threshold, red_threshold]
+					thresholds = (self.cv_image[:,:,0] < bgr_threshold[0])  | (self.cv_image[:,:,1] < bgr_threshold[1]) | (self.cv_image[:,:,2] < bgr_threshold[2])
+					mark[thresholds] = [0,0,0]
 
-					
-				#gray = cv2.cvtColor(mark,cv2.COLOR_BGR2GRAY) 
-				#canny = cv2.Canny(gray, 5000, 1500, apertureSize = 5, L2gradient = True)
-				#lines = cv2.HoughLinesP(canny, 0.8, np.pi / 180, 90, minLineLength = 10, maxLineGap = 100)
-
-				#for i in lines:
-				#	if abs(i[0][1] - i[0][3]) < 20: #y pixel diff
-				#		cv2.line(image, (i[0][0], i[0][1]), (i[0][2], i[0][3]), (255, 0, 0), 2)
-
-				#cv2.imshow("test", mark)
-				#cv2.imshow("test2", canny)
-				#cv2.imshow("test3", image)
-				#cv2.waitKey(0)
-				#cv2.destroyAllWindows()
-				if self.lineimage_d == 1
+					print('m112222222')
+					gray = cv2.cvtColor(mark,cv2.COLOR_BGR2GRAY) 
+					canny = cv2.Canny(gray, 5000, 1500, apertureSize = 5, L2gradient = True)
+					lines = cv2.HoughLinesP(canny, 0.8, np.pi / 180, 90, minLineLength = 10, maxLineGap = 100)
+					if type(lines) != type(None) :
+						for i in lines:
+							if abs(i[0][1] - i[0][3]) < 20: #y pixel diff
+								cv2.line(self.cv_image, (i[0][0], i[0][1]), (i[0][2], i[0][3]), (255, 0, 0), 2)
+				
+					cv2.imshow("test", self.cv_image)
+					cv2.imshow("test2", canny)
+					cv2.imshow("test3", mark)
+					cv2.waitKey(1)
+				if self.lineimage_d == 1:
 					self.count +=1
 					print(self.count)
-					if self.count == 30
+					if self.count == 30:
 						self.mode = 0
 		elif self.mode == 5:
 				print('mode 5')
